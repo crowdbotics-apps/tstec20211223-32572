84c805f3552133e429550bce661eb9b487aa41a6
Crowdbotics <team@crowdbotics.com>
2021-12-24 00:37:51 +0000

    Initial commit.

---
+++ backend/tstec20211223_32572/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for tstec20211223_32572 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tstec20211223_32572.settings')
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "tstec20211223_32572",
+  url: "https://tstec20211223_32572.botics.co",
+  api: "https://tstec20211223_32572.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
